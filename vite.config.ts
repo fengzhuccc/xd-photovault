0387cf0fce13344481ce11dace7f964dacb8394b
ss ssik <fengzhutingsky@163.com>
2026-07-12 17:22:13 +0800

    refactor: 关于页面版本号从 package.json 自动读取

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
+import packageJson from './package.json' with { type: 'json' };
 
 export default defineConfig({
   base: './',
+  define: {
+    __APP_VERSION__: JSON.stringify(packageJson.version),
+  },
   build: {
     sourcemap: 'hidden',
     outDir: 'dist',
